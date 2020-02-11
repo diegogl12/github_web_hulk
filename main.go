e5451aab17e48bed33c8b8ab56f0e6fd31729117
Stevan Olegario <stevan.olegario@gmail.com>
2020-02-11 19:50:07 -0300

    adding .env to app

--- main.go
+++ main.go
@@ -4,31 +4,38 @@ import (
 	"fmt"
 	services "./services"
 	"./structs"
-	"flag"
 	"regexp"
+	"log"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
-	owner := flag.String("owner", "diegogl12", "foo")
-	repos_pointer := flag.String("repos", "", "foo")
-	flag.Parse()
-
+	LoadEnv()
+	owner := os.Getenv("OWNER")
+	repos_pointer := os.Getenv("REPOS")	
 	repos := parseRepos(repos_pointer)
 	var branch_list [] structs.Branch
 
 	for _, repo := range repos {
-		branch := services.ParsePayload(*owner, repo)
+		branch := services.ParsePayload(owner, repo)
 		fmt.Printf("%+v",branch)
 		branch_list = append(branch_list, branch)
 	}
-	
+
 	services.CreateAndWriteCsv(branch_list)
 }
 
-func parseRepos(repos *string) []string{
-	params := regexp.MustCompile(",")
-	result_repos := params.Split(*repos, -1)
+func parseRepos(repos string) []string{
+	params := regexp.MustCompile(", ")
+	result_repos := params.Split(repos, -1)
 
 	return result_repos
 }
 
+func LoadEnv() {
+    // loads values from .env into the system
+    if err := godotenv.Load(); err != nil {
+        log.Print("No .env file found")
+    }
+}
--- services/api_service.go
+++ services/api_service.go
@@ -6,6 +6,7 @@ import (
 	"../structs"
 	"net/http"
 	"io/ioutil"
+	"os"
 )
 
 func ParsePayload(owner string, repo string) structs.Branch{
@@ -14,18 +15,18 @@ func ParsePayload(owner string, repo string) structs.Branch{
 	bodyBytes := callAPI(owner, repo)
 
 	err := json.Unmarshal(bodyBytes, &branch)
-	
+
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	
+
 	return branch
 }
 
 func callAPI(owner string, repo string) []byte{
-	accessToken := "e5688e0f8051160379fb860856878963fb7dd254"
+	accessToken := os.Getenv("TOKEN")
 	token := "token " + accessToken
-
+	
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
@@ -33,6 +34,7 @@ func callAPI(owner string, repo string) []byte{
 	}
 
 	url := "https://api.github.com/repos/" + owner + "/" + repo + "/branches/master"
+
 	query, _ := http.NewRequest("GET", url, nil)
 
 	parameters := query.URL.Query()
